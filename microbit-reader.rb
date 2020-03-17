956fc1660100216e0da298b5a1e5c42981f2b22c
Glen Pike <github@glenpike.co.uk>
2020-03-17 23:44:17 +0000

    Streaming pitch/roll/compass to distortion/cutoff/resonance
    Receive data from microbit and map to controllers for the Bass Station.
    
    TODO:
    Add in Microbit code here.
    Add ranges / clamping of values so we can get a finer control
    Allow Microbit to send values, but decide in here whether to throttle.

--- microbit-reader.rb
+++ microbit-reader.rb
@@ -1,19 +1,64 @@
 require 'serialport'
+require 'json'
+require "unimidi"
 
-PORT = '/dev/cu.usbmodem1431202'
+PORT = '/dev/cu.usbmodem1434402'
 BAUD = 115200
 
 serial = SerialPort.new(PORT, BAUD, 8, 1, SerialPort::NONE)
+
+output = UniMIDI::Output.gets
 
 def print_exception(exception, explicit)
   puts "[#{explicit ? 'EXPLICIT' : 'INEXPLICIT'}] #{exception.class}: #{exception.message}"
   puts exception.backtrace.join("\n")
 end
 
+def map180(value)
+  ((value * 1.0 + 180) * ( 127.0 / 360.0)).to_i
+end
+
+midi_map = {
+  cutoff: 16,
+  resonance: 82,
+  modulation: 1,
+  overdrive: 114,
+  distortion: 94
+}
+
+control_map = {
+  roll: :cutoff,
+  compass: :resonance,
+  pitch: :distortion
+}
+
 begin
   loop do
     line = serial.readline()
-    puts "line: #{line}"
+    begin
+      packet = JSON.parse(line)
+      # byebug
+      name = packet['n']
+    
+      control = control_map[name.to_sym]
+      if control
+        cc = midi_map[control]
+        value = packet['v']
+        
+        puts "#{control} = #{cc} : #{value} = #{map180(value)}"
+        output.puts(176, cc, value) 
+      else
+        if name.to_s == "input"
+          puts "name! #{packet['v']}"
+        # else
+          # puts "name: #{name} packet #{packet}"
+        end
+      end
+    rescue JSON::ParserError => e
+      puts "invalid packet: #{line}"
+    rescue NoMethodError
+      next
+    end
   end
 rescue Interrupt => e
   print_exception(e, true)
