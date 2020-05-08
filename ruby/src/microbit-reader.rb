1cd2b9f6e5708afe80fc387cb5ab9083c691e11f
Glen Pike <github@glenpike.co.uk>
2020-05-08 17:42:23 +0100

    Hacking with 2 microbit transmitters

--- ruby/src/microbit-reader.rb
+++ ruby/src/microbit-reader.rb
@@ -42,12 +42,32 @@ def print_exception(exception, explicit)
   puts exception.backtrace.join("\n")
 end
 
+# -35 to 35
+mapRoll = lambda { |value|
+  min = -55
+  max = 55
+  offset = 180
+  input = [value, min].max
+  input = [input, max].min
+  ((value.to_f + offset) * ( 127.0 / (max - min).to_f)).to_i
+}
+
 map180 = lambda { |value|
   ((value * 1.0 + 180) * ( 127.0 / 360.0)).to_i
 }
 
 map360 = lambda { |value|
   ((value * 1.0) * ( 127.0 / 360.0)).to_i
+}
+
+notes_1 = [36, 40, 43, 48, 52, 55, 60, 64, 67] # C E G arpeggios
+notes_2 = [36, 38, 40, 41, 43, 45, 47, 48].map { |n| n + 24 }
+
+mapNotes = lambda { |value, notes|
+  input = [value, 0].max
+  input = [input, 100].min
+  index = ((input.to_f / 101) * notes.length).to_i
+  notes[index]
 }
 
 midi_map = {
@@ -58,12 +78,45 @@ midi_map = {
   distortion: 94
 }
 
-control_map = {
-  roll: { cc: :cutoff, fn: map180 },
-  compass: { cc: :resonance, fn: map360 },
-  pitch: { cc: :distortion, fn: map180 }
+control_map_1 = {
+  roll: { cc: :cutoff, fn: mapRoll },
+  # compass: { cc: :distortion, fn: map360 },
 }
 
+control_map_2 = {
+  roll: { cc: :cutoff, fn: mapRoll },
+  # compass: { cc: :distortion, fn: map360 },
+}
+
+note_map_1 = {
+  pitch: { fn: mapNotes, notes: notes_1 }
+}
+
+note_map_2 = {
+  pitch: { fn: mapNotes, notes: notes_2 }
+}
+
+last_note = nil
+
+id_map = {
+  1901794473 => {
+    channel: 0,
+    note_map: note_map_1,
+    control_map: control_map_1,
+    last_note: nil
+  },
+  1689304789 => {
+    channel: 1,
+    note_map: note_map_2,
+    control_map: control_map_2,
+    last_note: nil
+  }
+}
+id_map.each { |k, v| puts "keys: '#{k}'" }
+puts "id map: #{id_map[1689304789].inspect}"
+
+output.puts(0x91, 36, 100)
+output.puts(0x92, 48, 100)
 begin
   loop do
     line = serial.readline()
@@ -71,15 +124,38 @@ begin
       packet = JSON.parse(line)
       # byebug
       name = packet['n']
+      
+      
+      controller = id_map[packet['s'].to_i]
+      # puts " controller for '#{packet['s'].to_i}': #{controller.inspect}"
+      unless controller
+        next
+      end
 
+      control_map = controller[:control_map]
+      note_map = controller[:note_map]
+      channel = controller[:channel] || 0
       control = control_map[name.to_sym]
+      note = note_map[name.to_sym]
       if control
         cc = midi_map[control[:cc]]
         fn = control[:fn]
         value = fn.call(packet['v'])
         
-        puts "#{cc} (#{control[:cc]}) : #{packet['v']} = #{value}"
-        output.puts(176, cc, value) 
+        # puts "#{cc} (#{control[:cc]}) : #{packet['v']} = #{value}"
+        output.puts(176 + channel, cc, value) 
+      elsif note
+        fn = note[:fn]
+        value = fn.call(packet['v'], note[:notes])
+        puts "#{name} channel: #{channel} msg: #{0x90 + channel} note: #{packet['v']} = #{value}"
+        if controller[:last_note]
+          output.puts(0x80 + channel, controller[:last_note], 0)
+        end
+        if controller[:last_note] != value
+          output.puts(0x90 + channel, value, 100)
+          controller[:last_note] = value
+        end
+        
       else
         if name.to_s == "input"
           puts "name! #{packet['v']}"
@@ -99,4 +175,10 @@ rescue SignalException => e
   print_exception(e, false)
 rescue StandardError => e
   print_exception(e, false)
+end
+
+id_map.each do |c|
+  if c[:last_note]
+    output.puts(0x80 + c[:channel], c[:last_note], 0)
+  end
 end
