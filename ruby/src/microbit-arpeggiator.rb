ef94497ab27eb6c5c77f6dab53ccdb897e8fcbed
Glen Pike <github@glenpike.co.uk>
2021-11-24 14:17:07 +0000

    Change to use printf which is supposed to be better for threads + io

--- ruby/src/microbit-arpeggiator.rb
+++ ruby/src/microbit-arpeggiator.rb
@@ -16,9 +16,9 @@ end
 files = Dir[PORT_SEARCH]
 
 if files.length > 1
-  puts "What port is your Microbit on?\nChoose a number\n"
+  printf "What port is your Microbit on?\nChoose a number\n\n"
   files.each_with_index do |f, index|
-    puts "#{index + 1}: #{f}"
+    printf "#{index + 1}: #{f}\n"
   end
   choice = gets
   choice = choice.chomp.to_i
@@ -27,19 +27,19 @@ else
   PORT = files.first
 end
 
-puts "PORT: #{PORT}"
+printf "PORT: #{PORT}\n"
 
 BAUD = 115_200
 
 serial = SerialPort.new(PORT, BAUD, 8, 1, SerialPort::NONE)
 
-UniMIDI::Output.all.each { |d| puts d.inspect.to_s }
+UniMIDI::Output.all.each { |d| printf d.inspect.to_s }
 
 output = UniMIDI::Output.gets
 
 def print_exception(exception, explicit)
-  puts "[#{explicit ? 'EXPLICIT' : 'INEXPLICIT'}] #{exception.class}: #{exception.message}"
-  puts exception.backtrace.join("\n")
+  printf "[#{explicit ? 'EXPLICIT' : 'INEXPLICIT'}] #{exception.class}: #{exception.message}\n"
+  printf exception.backtrace.join("\n")
 end
 
 transpose = lambda { |value|
@@ -65,8 +65,8 @@ id_map = {
     last_note: nil
   }
 }
-id_map.each { |k, _v| puts "keys: '#{k}'" }
-# puts "id map: #{id_map[1_689_304_789].inspect}"
+id_map.each { |k, _v| printf "keys: '#{k}'\n" }
+# printf "id map: #{id_map[1_689_304_789].inspect}\n"
 
 options = {
   gate: 90,
@@ -84,7 +84,7 @@ clock << arpeggiator
 chord = %w[C3 G3 E3]
 
 arpeggiator.add(chord)
-puts 'starting clock'
+printf "starting clock\n"
 clock.start(focus: true)
 
 # Can't seem to use normal readline with threads as it blocks
@@ -102,55 +102,73 @@ rescue IO::EAGAINWaitReadable
   retry
 end
 
+def microbit_to_midi
+  printf "poo\n"
+  sleep 1
+  true
+  # line = readline_nonblock(serial)
+  # packet = JSON.parse(line)
+  # # byebug
+  # name = packet['n']
+
+  # controller = id_map[packet['s'].to_i]
+  # # printf " controller for '#{packet['s'].to_i}': #{controller.inspect}\n"
+  # return unless controller
+
+  # note_map = controller[:note_map]
+  # channel = controller[:channel] || 0
+  # note = note_map[name.to_sym]
+  # if note
+  #   fn = note[:fn]
+  #   value = fn.call(packet['v'])
+  #   # printf "#{name} channel: #{channel} msg: #{0x90 + channel} note: #{packet['v']} = #{value}\n"
+  #   # output.puts(0x80 + channel, controller[:last_note], 0) if controller[:last_note]
+  #   if controller[:last_note] != value
+  #     # printf "#{name} channel: #{channel} msg: #{0x90 + channel} note: #{packet['v']} = #{value}\n"
+  #     # output.puts(0x90 + channel, value, 100)
+  #     arpeggiator.transpose = value
+  #     printf "arpeggiator.transpose #{arpeggiator.transpose}\n"
+  #     controller[:last_note] = value
+  #     clock.stop
+  #     clock.start(focus: true)
+  #   end
+
+  # elsif name.to_s == 'input'
+  #   printf "name! #{packet['v']}\n"
+  #   # else
+  #   #   printf "name: #{name} packet #{packet}\n"
+  # end
+end
+@thread
+
 begin
-  loop do
-    line = readline_nonblock(serial)
-    begin
-      packet = JSON.parse(line)
-      # byebug
-      name = packet['n']
-
-      controller = id_map[packet['s'].to_i]
-      # puts " controller for '#{packet['s'].to_i}': #{controller.inspect}"
-      next unless controller
-
-      note_map = controller[:note_map]
-      channel = controller[:channel] || 0
-      note = note_map[name.to_sym]
-      if note
-        fn = note[:fn]
-        value = fn.call(packet['v'])
-        # puts "#{name} channel: #{channel} msg: #{0x90 + channel} note: #{packet['v']} = #{value}"
-        # output.puts(0x80 + channel, controller[:last_note], 0) if controller[:last_note]
-        if controller[:last_note] != value
-          # puts "#{name} channel: #{channel} msg: #{0x90 + channel} note: #{packet['v']} = #{value}"
-          # output.puts(0x90 + channel, value, 100)
-          arpeggiator.transpose = value
-          puts "arpeggiator.transpose #{arpeggiator.transpose}"
-          controller[:last_note] = value
-          clock.stop
-          clock.start(focus: true)
-        end
-
-      elsif name.to_s == 'input'
-        puts "name! #{packet['v']}"
-        # else
-        #   puts "name: #{name} packet #{packet}"
-      end
-    rescue JSON::ParserError => e
-      # puts "invalid packet: #{line}"
-      next
-    rescue NoMethodError
-      puts 'NoMethodError'
-      next
-    end
+  @thread = Thread.new do
+    loop { microbit_to_midi }
+  rescue JSON::ParserError => e
+    # printf "invalid packet: #{line}\n"
+    next
+  rescue NoMethodError
+    printf "NoMethodError\n"
+    next
+  rescue Interrupt => e
+    printf 'Thread interrupt?'
+    Thread.main.raise(e)
+  rescue StandardError => e
+    printf 'Thread exception?'
+    Thread.main.raise(e)
   end
+  @thread.abort_on_exception = true
+  @thread.run
 rescue Interrupt => e
   print_exception(e, true)
 rescue SignalException => e
   print_exception(e, false)
 rescue StandardError => e
   print_exception(e, false)
+ensure
+  printf "stopping clock and joining thread\n"
+  @thread.join
+  clock.stop
 end
 
 id_map.each do |c|
